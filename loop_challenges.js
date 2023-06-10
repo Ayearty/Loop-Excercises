5cde66fc0f3cfe287050c0b18294bb510f1efb69
Alexia Yearty <122060153+Ayearty@users.noreply.github.com>
2023-06-09 21:44:47 -0700

    Add files via upload

---
+++ loop_challenges.js
@@ -0,0 +1,32 @@
+// Challenge 1
+for (var i = 1; i<=20; i++){
+    if (!(i % 2 == 0)){
+    console.log (i)
+    }
+}
+
+// Challenge 2
+for (i = 100; i >= 0; i--){
+    if (i % 3 == 0){
+    console.log (i)
+    }
+}
+
+//Challenge 3
+for (i = 4; i >= -3.5; i -= 1.5){
+    console.log(i)
+}
+
+//Challenge 4
+var x = 0
+for (i = 0; i <= 100; i++){
+    x = x + i
+}
+console.log (x)
+
+//Challenge 5
+x = 1
+for (i = 1; i <= 12; i++){
+    x = x * i
+}
+console.log(x)
